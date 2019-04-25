abdcc7ef32d363383882fb3c98d80b01dee031e8
sasoria <sergio_s86@hotmail.com>
2019-04-25 23:12:21 +0200

    Removed header

--- js/apiService.js
+++ js/apiService.js
@@ -5,7 +5,6 @@ app.factory("apiService", function($http, $q) {
     getStations: function(api_endpoint) {
       var xhReq = new XMLHttpRequest();
       xhReq.open("GET", base_url+api_endpoint, false)
-      xhReq.setRequestHeader("Content-type", "text/plain")
       xhReq.send(null)
       
       return JSON.parse(xhReq.responseText).data.stations
